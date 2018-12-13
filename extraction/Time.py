640a5241de04e00436196cc4ac99baae8194a5bd
Hamza Keurti <hamza.keurti@gmail.com>
2018-12-13 21:33:15 +0800

    Corrected issue with values filled. Can only fill na with values from average day.

--- extraction/Time.py
+++ extraction/Time.py
@@ -13,17 +13,18 @@ def format_timestamp(dataframe):
     """Convert timestamp column to datetime object.
     Input dataframe should have timestamp column in int format."""
     dataframe.timestamp = dataframe.timestamp.apply(lambda x: datetime.datetime.fromtimestamp(x))
- 
-    
+
+
 def extract_seasonal_time(dataframe):
     """Adds columns:
     minute (minutes of the day 0 to 60*24-1)
     and day (day of the week 0 to 6).
     Input dataframe should have timestamp column in datetime format,
     Use format_timestamp(dataframe)"""
-    dataframe["minute"] = dataframe.timestamp.apply(lambda x: x.minute + 60 * x.hour)
-    dataframe["day"] = dataframe.timestamp.apply(lambda x: x.weekday())
-    dataframe["minute_of_week"] = dataframe.minute + 60*24*dataframe.day
+    dataframe["minute"] = get_minute_of_day_column(dataframe.timestamp)
+    dataframe["day"] = get_day_of_week_column(dataframe.timestamp)
+    dataframe["minute_of_week"] = dataframe.minute + 60 * 24 * dataframe.day
+
 
 # TODO probs move this out of Time file, into util
 def split_on_id(dataframe):
@@ -32,11 +33,19 @@ def split_on_id(dataframe):
     return {x: grouped_df.get_group(x) for x in grouped_df.groups}
 
 
-def fill_nas(single_KPI,not_anomaly=False,period = "week"):
+def get_minute_of_day_column(timestamps):
+    return timestamps.apply(lambda x: x.minute + 60 * x.hour)
+
+
+def get_day_of_week_column(timestamps):
+    return timestamps.apply(lambda x: int(x.weekday()))
+
+
+def fill_nas(single_KPI, not_anomaly=False):
     """Pass a single KPI in, which timestamps column is already converted to datetime format"""
     # We need minutes column to proceed.
     extract_seasonal_time(single_KPI)
-        
+
     single_KPI.index = pd.DatetimeIndex(single_KPI.timestamp)
 
     start = single_KPI.head(1).timestamp.iloc[0]
@@ -45,7 +54,7 @@ def fill_nas(single_KPI,not_anomaly=False,period = "week"):
 
     indices = pd.date_range(start, end, freq=gap)
 
-    single_KPI["imputed"] = 0 #Whether or not the row is imputed
+    single_KPI["imputed"] = 0  # Whether or not the row is imputed
     single_KPI = single_KPI.reindex(indices)
     values_replace_na = {
         "KPI ID": single_KPI["KPI ID"].iloc[0],
@@ -58,26 +67,25 @@ def fill_nas(single_KPI,not_anomaly=False,period = "week"):
     # For each missing value at minute i of the week,
     # we will use the mean over all available values at same minute of different weeks
     # Note: we need times
-    groupby_column = ""
-    if period == "day":
-        groupby_column = "minute"
-    if period == "week":
-        groupby_column = "minute_of_week"
-    
-    if not_anomaly:#We can choose to only consider non anomalous values in the computing of means.
-        means = single_KPI[single_KPI.label == 0].groupby([groupby_column])["value"].mean()
-    else:#We cannot select non anomalous during testing. (TODO: In case of testing fill with means from training)
-        means = single_KPI.groupby([groupby_column])["value"].mean()
 
-    #assert len(means)*gap.minute + gap.hour*60 == 60*24 # Check if we have a value for each minute of the day
+    if not_anomaly:  # We can choose to only consider non anomalous values in the computing of means.
+        means = single_KPI[single_KPI.label == 0].groupby(["minute"])["value"].mean()
+    else:  # We cannot select non anomalous during testing. (TODO: In case of testing fill with means from training)
+        means = single_KPI.groupby(["minute"])["value"].mean()
+
+    # assert len(means)*gap.minute + gap.hour*60 == 60*24 # Check if we have a value for each minute of the day
     single_KPI.timestamp = single_KPI.index
     na_KPI = single_KPI[single_KPI.imputed == 1]
-    
-    
-    
-    new_values = na_KPI[groupby_column].apply(lambda x: means[x])
+    extract_seasonal_time(na_KPI)
+
+    new_values = na_KPI["minute"].apply(lambda x: means[x])
+    values_replace_na = {
+        "value": new_values,
+        "minute": na_KPI.minute.values,
+        "day": na_KPI.day.values,
+        "minute_of_week": na_KPI.minute_of_week
+    }
     single_KPI.value = single_KPI.value.fillna(new_values)
-    extract_seasonal_time(single_KPI)
     return single_KPI
 
 
@@ -98,7 +106,7 @@ def remove_imputed_samples(ids_predictions, beefed_data):
     for id in ids_predictions:
         pred = ids_predictions[id]
         imputed = beefed_data[id].imputed
-        both = list(zip(pred,imputed))
+        both = list(zip(pred, imputed))
         unbeefed = [pr for (pr, im) in both if not im]
         ids_predictions[id] = pd.Series(unbeefed)
     return ids_predictions
